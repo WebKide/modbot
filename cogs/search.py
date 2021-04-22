eeb12c7acf1d9b871ee29d1ad09fd79258258a64
kuro <46526001+WebKide@users.noreply.github.com>
2021-04-22 06:10:19 +0100

    Update search.py

--- cogs/search.py
+++ cogs/search.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 class Search:
     def __init__(self, bot):
         self.bot = bot
-        self.session = requests.Session()
+        #  self.session = requests.Session()
         self.headers = {
             'User-Agent': 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR '
                           '2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; MS-RTC LM 8; '
