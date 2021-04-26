312e6acbd266afd668bef986e90ae49f074d1a5b
kuro <46526001+WebKide@users.noreply.github.com>
2021-04-26 19:33:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ class ModBot(commands.Bot):
         self.add_command(self.unload)
         self.startup_ext = [x.stem for x in Path('cogs').glob('*.py')]
         self._extensions = [x.replace('.py', '') for x in os.listdir('cogs') if x.endswith('.py')]
-        # self.run(os.getenv('TOKEN').strip('\"'))
+        self.run(os.getenv('TOKEN').strip('\"'))
         self.version = __version__
         self.load_extensions()  # automatically loads plugins inside /cogs/
         self.loop.create_task(self.periodic_presence_change())  # create presence-loop
